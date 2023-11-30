add0064f2233533717d7e21e94f894041f26f421
Kidus1343 <84654265+Kidus1343@users.noreply.github.com>
2023-11-30 18:00:47 +0300

    update DBContext

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeesManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions): base(contextOptions) 
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNo { get; set; }
+    }
+}
